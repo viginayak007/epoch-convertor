8abe84e75b5f8dc83fd76d64a236cfd0e4144348
viginayak007 <viginayak007@gmail.com>
2021-12-28 18:12:11 +0530

    1 file modified, 2 added

---
+++ src/app/multi/multi.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+@Component({
+  selector: 'app-multi',
+  templateUrl: './multi.component.html',
+  styleUrls: ['./multi.component.css'],
+})
+export class MultiComponent implements OnInit {
+  constructor() {
+    var d = new Date(1458619200000);
+    var myTimezone = 'America/Toronto';
+    var myDatetimeFormat = 'YYYY-MM-DD hh:mm:ss a z';
+    var myDatetimeString = moment(d).tz(myTimezone).format(myDatetimeFormat);
+    console.log(myDatetimeString);
+  }
+
+  ngOnInit() {}
+}
