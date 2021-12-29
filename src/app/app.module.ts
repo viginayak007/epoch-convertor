61ed9b57301869e6d60677b06b0ab97d93558f8a
viginayak007 <viginayak007@gmail.com>
2021-12-29 11:49:56 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MultiComponent } from './multi/multi.component';
 import { ConvertService } from './convert.service';
+import { ConvertorComponent } from './convertor/convertor.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, MultiComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    MultiComponent,
+    ConvertorComponent,
+  ],
   bootstrap: [AppComponent],
   providers: [ConvertService],
 })
